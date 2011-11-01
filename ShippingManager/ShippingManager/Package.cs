0e4f79666ff284b99982097af2cef9577e887353
Lotar Walace <kimbokasteniv@gmail.com>
2011-10-31 19:58:02 -0500

    added enums for package

--- ShippingManager/ShippingManager/Package.cs
+++ ShippingManager/ShippingManager/Package.cs
@@ -7,5 +7,26 @@ namespace ShippingManager
 {
     public class Package
     {
+        public enum SERVICE_TYPE
+        {
+            Economy,
+            Priority,
+            Express,
+            NextDay
+        }
+
+        public enum WEIGHT_CLASS
+        {
+            _1oz_8oz,
+            _9oz_1lb,
+            _1lb_2lb,
+            _2lb_3lb,
+            _3lb_5lb,
+            _5lb_8lb,
+            _8lb_15lb,
+            _15lb_25lb,
+            _25lb_50lb,
+            _50lb_greater
+        }
     }
 }
