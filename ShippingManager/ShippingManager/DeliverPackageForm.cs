66cb0ddccd34f76aaf0e68dc8a2c688add71ad1c
Lotar Walace <kimbokasteniv@gmail.com>
2011-10-31 21:09:51 -0500

    added deliveryPackageForm

---
+++ ShippingManager/ShippingManager/DeliverPackageForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShippingManager
+{
+    public partial class DeliverPackageForm : Form
+    {
+        public DeliverPackageForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
