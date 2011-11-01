91ec36080ef9bf9537b51732edcc2bad42dd886d
Lotar Walace <kimbokasteniv@gmail.com>
2011-10-31 19:42:36 -0500

    oops forgot these skeleton classes

---
+++ ShippingManager/ShippingManager/AddPackageForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShippingManager
+{
+    public partial class AddPackageForm : Form
+    {
+        public AddPackageForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
